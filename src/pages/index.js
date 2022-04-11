91f8b713baf22cc7593b2e45b0a0ab8d1bbdfd05
Remy Apfelbacher <remy.apfelbacher@contabo.de>
2022-04-11 14:45:50 +0200

    fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/products">
+            to="/docs/products/intro">
             Getting Started with Contabo Products - ⏱️
           </Link>
         </div>
